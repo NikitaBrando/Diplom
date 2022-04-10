f7bcc86208d37202d846cf6a80a82f57a6135e15
NikitaBrando <73003645+NikitaBrando@users.noreply.github.com>
2022-04-10 22:22:14 +0900

    Add files via upload

---
+++ about.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Lena Pillars</title>
+	<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="Images/favicon.ico" type="image/x-icon">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Kanit:wght@200&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="sytle/stylecss2.css">
+</head>
+<body>
+		
+	<?php 
+    require "blocks/header2.php"
+  	?>
+		
+	<main>
+	<div class="container">
+  
+  	<h2>Контакты</h2>
+  	<div class="contact">
+  	<div class="contact_cards">
+  	<p>Директор</p>
+  	<p>Калитин Василий Николаевич</p>
+  	<p>Телефон: 8 (4114) 445289<br>
+E-mail: nppls@mail.ru</p>
+  	</div>
+  	<div class="contact_cards">
+  	<p>Заместитель директора</p>
+  	<p>Пономарев Валерий Валентинович</p>
+  	<p>Телефон: 8 (4114) 445289<br>
+E-mail: nppls@mail.ru</p>
+  	</div>
+  	<div class="contact_cards">
+  	<p>Главный бухгалтер</p>
+  	<p>Кононова Степанида Спиридоновна</p>
+  	<p>Телефон: 8 (4114) 445487<br>
+E-mail: nppls@mail.ru</p>
+  	</div>
+  	<div class="contact_cards">
+  	<p>Юрист - делопроизводитель</p>
+  	<p>Кузьмина Надежда Ануфриевна</p>
+  	<p>Телефон: 8 (4114) 445289<br>
+E-mail: nppls@mail.ru</p>
+  	</div>
+  	<div class="contact_cards">
+  	<p>Начальник инспекции</p>
+  	<p>Давыдова Вероника Васильевна</p>
+  	<p>Телефон: 8 (4114) 445289<br>
+E-mail: nppls@mail.ru</p>
+  	</div>
+  	<div class="contact_cards">
+  	<p>Специалист по туризму</p>
+  	<p>Корякина Елена Михайловна</p>
+  	<p>Телефон: 8 (4114) 445289<br>
+E-mail: nppls@mail.ru</p>
+  	</div>
+  </div>
+</div>
+	</main>
+	
+	<?php 
+    require "blocks/footer.php"
+  	?>
+  	
+</body>
+</html>
